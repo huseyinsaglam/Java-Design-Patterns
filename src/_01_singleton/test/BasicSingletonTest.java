36b38dea0e5105759a95a2733fd1db61c8a2cb0f
ETIYACORP\huseyin.saglam <huseyin.saglam@etiya.com>
2021-06-24 23:03:57 +0300

    Basic singleton write example

---
+++ src/_01_singleton/test/BasicSingletonTest.java
@@ -0,0 +1,28 @@
+package _01_singleton.test;
+
+import _01_singleton.BasicSingleton;
+
+public class BasicSingletonTest {
+    public static void main(String[] args) {
+
+        for (int i=0;i<10;i++){
+            BasicSingleton singleton = BasicSingleton.getBasicSingleton();
+            singleton.printName();
+
+        }
+
+        BasicSingleton s1 = BasicSingleton.getBasicSingleton();
+        BasicSingleton s2 = BasicSingleton.getBasicSingleton();
+
+        if (s1.hashCode() == s2.hashCode()) {
+            System.out.println(s1.hashCode());
+            System.out.println(s2.hashCode());
+            System.out.println("s1 ve s2 objeleri aynidir");
+        } else {
+            System.out.println(s1.hashCode());
+            System.out.println(s2.hashCode());
+            System.out.println("s1 ve s2 objeleri farkidir");
+        }
+
+    }
+}
