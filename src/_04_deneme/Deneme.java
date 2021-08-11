0f6ebac0a66e4eb21f4e133cc1c9b52050520290
hüseyin <hsaglam001@gmail.com>
2021-08-11 21:22:11 +0300

    Feature/deneme (#6)
    * selamBir metot oluşturuldu
    
    * selamiki metot oluşturuldu
    
    Co-authored-by: ETIYACORP\huseyin.saglam <huseyin.saglam@etiya.com>

---
+++ src/_04_deneme/Deneme.java
@@ -0,0 +1,15 @@
+package _04_deneme;
+
+public class Deneme {
+    public static void main(String[] args) {
+        selamBir();
+    }
+
+    public static void selamBir() {
+        System.out.println("Selam commit bir");
+    }
+
+    public static void selamiki() {
+        System.out.println("Selam commit iki");
+    }
+}
