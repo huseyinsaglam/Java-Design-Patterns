22e278234b5f3b62b732dcd1471d36419876d80a
ETIYACORP\huseyin.saglam <huseyin.saglam@etiya.com>
2021-08-11 21:08:34 +0300

    selamBir metot oluşturuldu

---
+++ src/_04_deneme/Deneme.java
@@ -0,0 +1,11 @@
+package _04_deneme;
+
+public class Deneme {
+    public static void main(String[] args) {
+        selamBir();
+    }
+
+    public static void selamBir() {
+        System.out.println("Selam commit bir");
+    }
+}
