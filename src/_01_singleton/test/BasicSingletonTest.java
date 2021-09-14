56c6266df4c3d1c4fb59e8ad56e3a8017195051e
ETIYACORP\huseyin.saglam <huseyin.saglam@etiya.com>
2021-09-14 22:06:38 +0300

    fixed

--- src/_01_singleton/test/BasicSingletonTest.java
+++ src/_01_singleton/test/BasicSingletonTest.java
@@ -22,6 +22,7 @@ public class BasicSingletonTest {
             System.out.println(s1.hashCode());
             System.out.println(s2.hashCode());
             System.out.println("s1 ve s2 objeleri farkidir");
+
         }
 
     }
