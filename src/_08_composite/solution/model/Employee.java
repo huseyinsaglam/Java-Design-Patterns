e010f97fa9e7a6b23497da7fbb83cc45c5a821db
ETIYACORP\huseyin.saglam <huseyin.saglam@etiya.com>
2022-01-17 23:46:15 +0300

    composite Design Pattern

---
+++ src/_08_composite/solution/model/Employee.java
@@ -0,0 +1,49 @@
+package _08_composite.solution.model;
+
+public class Employee {
+
+    private String name;
+    private String surname;
+    private long id;
+
+    public Employee(String name, String surname, long id) {
+        this.name = name;
+        this.surname = surname;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+
+}
