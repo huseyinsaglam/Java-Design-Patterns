879def0f79a50c23a7a65aa680e9ba5c261259f3
hüseyin <hsaglam001@gmail.com>
2022-09-11 16:44:26 +0300

    Feature/composite (#23)
    * composite design pattern example 1
    
    * composite design pattern
    
    Co-authored-by: ETIYACORP\huseyin.saglam <huseyin.saglam@etiya.com>

---
+++ src/structural/_08_composite/pojo/CompositePatternEmployee.java
@@ -0,0 +1,36 @@
+package structural._08_composite.pojo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompositePatternEmployee {
+
+    private String name;
+    private String dept;
+    private int salary;
+    private List<CompositePatternEmployee> subordinates;
+
+    // constructor
+    public CompositePatternEmployee(String name,String dept, int sal) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = sal;
+        subordinates = new ArrayList<CompositePatternEmployee>();
+    }
+
+    public void add(CompositePatternEmployee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(CompositePatternEmployee e) {
+        subordinates.remove(e);
+    }
+
+    public List<CompositePatternEmployee> getSubordinates(){
+        return subordinates;
+    }
+
+    public String toString(){
+        return ("CompositePatternEmployee :[ Name : " + name + ", dept : " + dept + ", salary :" + salary+" ]");
+    }
+}
