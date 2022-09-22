47ec65fd0a0901ab16e6c780d0719f6ede090956
hüseyin <hsaglam001@gmail.com>
2022-09-22 23:49:43 +0300

    Facade Design Pattern (#25)
    Co-authored-by: ETIYACORP\huseyin.saglam <huseyin.saglam@etiya.com>

---
+++ src/structural/_10_facade/pojo/Circle.java
@@ -0,0 +1,8 @@
+package structural._10_facade.pojo;
+
+public class Circle implements Shape {  // daire/cember
+    @Override
+    public void draw() {
+        System.out.println("Circle drawing");
+    }
+}
