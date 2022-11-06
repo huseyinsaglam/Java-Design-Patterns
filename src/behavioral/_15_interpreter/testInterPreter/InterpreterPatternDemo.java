fef6e4ccfe2c6c6860bdc451390c9f0f34e81d3e
ETIYACORP\huseyin.saglam <huseyin.saglam@etiya.com>
2022-11-06 14:23:48 +0300

    Interpreter Design Pattern

---
+++ src/behavioral/_15_interpreter/testInterPreter/InterpreterPatternDemo.java
@@ -0,0 +1,29 @@
+package behavioral._15_interpreter.testInterPreter;
+
+import behavioral._15_interpreter.interpreterPattern.AndExpression;
+import behavioral._15_interpreter.interpreterPattern.Expression;
+import behavioral._15_interpreter.interpreterPattern.OrExpression;
+import behavioral._15_interpreter.interpreterPattern.TerminalExpression;
+
+public class InterpreterPatternDemo {
+
+    public static Expression getMaleExpression(){
+        Expression robert = new TerminalExpression("Robert");
+        Expression john = new TerminalExpression("John");
+        return new OrExpression(robert, john);
+    }
+
+    public static Expression getMarriedWomanExpression(){
+        Expression julie = new TerminalExpression("Julie");
+        Expression married = new TerminalExpression("Married");
+        return new AndExpression(julie, married);
+    }
+
+    public static void main(String[] args) {
+        Expression isMale = getMaleExpression();
+        Expression isMarriedWoman = getMarriedWomanExpression();
+
+        System.out.println("John is male? " + isMale.interpret("John"));
+        System.out.println("Julie is a married women? " + isMarriedWoman.interpret("Married Julie"));
+    }
+}
